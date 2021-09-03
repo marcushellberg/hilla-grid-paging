0ac8fc154b2479f26cfe8a1a6c1f4b6c9b0af6ab
Marcus Hellberg <marcus@vaadin.com>
2021-09-03 13:50:36 -0700

    starting point

--- src/main/java/com/example/application/data/endpoint/PersonEndpoint.java
+++ src/main/java/com/example/application/data/endpoint/PersonEndpoint.java
@@ -1,52 +1,26 @@
 package com.example.application.data.endpoint;
 
-import com.example.application.data.entity.Person;
-import com.example.application.data.service.PersonService;
-
 import java.util.List;
-import java.util.Optional;
-
-import com.vaadin.flow.server.connect.Endpoint;
-
-import org.vaadin.artur.helpers.GridSorter;
-import org.springframework.data.domain.Page;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.artur.helpers.PagingUtil;
-import javax.validation.constraints.Email;
-import java.time.LocalDate;
-import javax.annotation.Nullable;
+import com.example.application.data.entity.Person;
+import com.example.application.data.service.PersonRepository;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.server.connect.Endpoint;
 
 @Endpoint
 @AnonymousAllowed
 public class PersonEndpoint {
+  private PersonRepository repo;
 
-    private PersonService service;
+  public PersonEndpoint(PersonRepository repo) {
+    this.repo = repo;
+  }
 
-    public PersonEndpoint(@Autowired PersonService service) {
-        this.service = service;
-    }
+  public List<Person> listAll() {
+    return repo.findAll();
+  }
 
-    public List<Person> list(int offset, int limit, List<GridSorter> sortOrder) {
-        Page<Person> page = service
-                .list(PagingUtil.offsetLimitTypeScriptSortOrdersToPageable(offset, limit, sortOrder));
-        return page.getContent();
-    }
-
-    public Optional<Person> get(Integer id) {
-        return service.get(id);
-    }
-
-    public Person update(Person entity) {
-        return service.update(entity);
-    }
-
-    public void delete(Integer id) {
-        service.delete(id);
-    }
-
-    public int count() {
-        return service.count();
-    }
+  public long count() {
+    return repo.count();
+  }
 
 }
